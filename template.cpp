b9af37a8a53cf4cacefbee70f3b2c047f9edf0a0
Shashank Raghupatro <59408465+raghupatro@users.noreply.github.com>
2022-05-15 16:06:58 +0530

    add template.cpp

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    cout << 'test' << endl;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+    freopen("d:\\cp\\codingpractice\\input.txt", "r", stdin);
+
+    freopen("d:\\cp\\codingpractice\\output.txt", "w", stdout);
+
+#endif // ONLINE_JUDGE
+
+    int t = 0;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
