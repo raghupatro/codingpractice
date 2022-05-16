0470fa0811d3f0e2935b55393df1f42116019c01
Shashank Raghupatro <59408465+raghupatro@users.noreply.github.com>
2022-05-17 01:11:47 +0530

    found reason for WA

--- dsa-sheet/arrays/9.cpp
+++ dsa-sheet/arrays/9.cpp
@@ -5,20 +5,25 @@ using namespace std;
 int maxProfit(vector<int> &prices)
 {
     int min = prices[0], max = prices[0], res = 0;
-    // 7 1 5 3 6 4
-    for (int &i : prices)
+    // 7 1 5 3 6 41
+    // cout << prices.size() << endl;
+    for (int i = 0; i < prices.size(); ++i)
     {
-        if (i < max)
+        if (prices[i] < max)
         {
             res += (max - min);
-            min = i;
-            max = i;
+            min = prices[i];
+            max = prices[i];
         }
-        if (i >= max)
+        if (prices[i] > max)
         {
-            max = i;
+            max = prices[i];
         }
     }
+    // need the below statement in actual solution, but worked without it in some
+    // instances because array had an extra 0 at the end, inputting proper array
+    // provides correct ans, hence needs the line below
+    res += (max - min);
     return res;
 }
 int main()
@@ -32,12 +37,17 @@ int main()
 #endif
 
     vector<int> nums;
+    // there is some error with this driver code, it automatically takes an extra "0" in input array
     while (cin)
     {
         int i = 0;
         cin >> i;
         nums.push_back(i);
     }
+    // for (int i = 0; i < nums.size(); ++i)
+    // {
+    //     cout << '"' << nums[i] << '"' << "\t";
+    // }
     cout << maxProfit(nums) << endl;
     return 0;
 }
