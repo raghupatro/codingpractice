22b7a181588345a5196349da2a4f26a66f195b7b
Shashank Raghupatro <59408465+raghupatro@users.noreply.github.com>
2022-05-20 11:32:13 +0530

    added mergeWithoutExtraSpace driver code

---
+++ dsa450/mergeWithoutExtraSpace.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+void merge(int arr1[], int arr2[], int n, int m)
+{
+    // code here
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("d:\\cp\\codingpractice\\input.txt", "r", stdin);
+    freopen("d:\\cp\\codingpractice\\output.txt", "w", stdout);
+#endif
+
+    int n = 4, m = 5;
+    int a[] = {1, 3, 5, 7};
+    int b[] = {0, 2, 6, 8, 9};
+    merge(a, b, n, m);
+    for (int i = 0; i < n; ++i)
+    {
+        cout << a[i] << "\t";
+    }
+    cout << endl;
+    for (int i = 0; i < m; ++i)
+    {
+        cout << b[i] << "\t";
+    }
+
+    return 0;
+}
