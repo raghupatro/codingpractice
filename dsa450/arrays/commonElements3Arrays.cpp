34a560cc9f237b8bc0e82cd6c3ee8a955c268f9c
Shashank Raghupatro <59408465+raghupatro@users.noreply.github.com>
2022-05-28 17:13:20 +0530

    solved commonElements3Array

--- dsa450/arrays/commonElements3Arrays.cpp
+++ dsa450/arrays/commonElements3Arrays.cpp
@@ -7,24 +7,30 @@ vector<int> commonElements(vector<int> &A, vector<int> &B, vector<int> &C, int n
     // code here.
     int i = 0, j = 0, k = 0;
     vector<int> res;
+    int justpushed = 0;
     while (i < n1 && j < n2 && k < n3)
     {
+        // cout << "i = " << i << " j = " << j << " k = " << k << endl;
         if (A[i] == B[j] and B[j] == C[k])
         {
-            res.push_back(A[i]);
+            if (i >= 0 and A[i] != justpushed)
+            {
+                res.push_back(A[i]);
+                justpushed = A[i];
+            }
             i++;
             j++;
             k++;
         }
-        else if (A[i] == min(A[i], min(B[i], C[i])))
+        else if (A[i] == min(A[i], min(B[j], C[k])))
         {
             i++;
         }
-        else if (B[i] == min(A[i], min(B[i], C[i])))
+        else if (B[j] == min(A[i], min(B[j], C[k])))
         {
             j++;
         }
-        else if (C[i] == min(A[i], min(B[i], C[i])))
+        else if (C[k] == min(A[i], min(B[j], C[k])))
         {
             k++;
         }
@@ -41,9 +47,33 @@ main()
     freopen("d:\\cp\\codingpractice\\output.txt", "w", stdout);
 #endif
 
-    vector<int> A{1, 2, 3, 4}, B{3, 4, 5, 6}, C{3, 4, 5};
+    // vector<int> A{1, 2, 3, 4}, B{3, 4, 5, 6}, C{3, 4, 5};
+    int p = 0, q = 0, r = 0;
+    cin >> p >> q >> r;
+    vector<int> A(p, 0), B(q, 0), C(r, 0);
+    for (int &i : A)
+        cin >> i;
+    for (int &i : B)
+        cin >> i;
+    for (int &i : C)
+        cin >> i;
     vector<int> res = commonElements(A, B, C, A.size(), B.size(), C.size());
-    cout << "output file" << endl;
+    // for (int &i : A)
+    // {
+    //     cout << i << "\t";
+    // }
+    // cout << endl;
+    // for (int &i : B)
+    // {
+    //     cout << i << "\t";
+    // }
+    // cout << endl;
+    // for (int &i : C)
+    // {
+    //     cout << i << "\t";
+    // }
+    // cout << endl;
+    // cout << "output file" << endl;
     for (int &i : res)
     {
         cout << i << "\t";
